a94f29d464b7f34b8b60a66824e2733fe22daa4a
volkanarslan01 <101024530+volkanarslan01@users.noreply.github.com>
2023-05-22 13:08:37 +0300

    Project user profile backend finish

--- Server/Routes/userRoutes.js
+++ Server/Routes/userRoutes.js
@@ -47,29 +47,51 @@ router.post("/login", async (req, res) => {
   });
 });
 
-router.post("/validate", async (req, res) => {
-  const { email, password } = req.body;
-  console.log(email, password);
-  emailValid = await userDB.findOne({ email });
-  const find = await userDB.find({ __v: 0 });
-  if (password != "") {
-    const isValid = await bcrypt.compare(password, emailValid.password);
-    if (!isValid) {
-      return res.send({ msg: "Does not match your old password" });
-    }
-  }
-  return res.send(emailValid._id);
-});
+// ? email current check
+// router.post("/validate", async (req, res) => {
+//   const { email, password } = req.body;
+//   console.log(email, password);
+//   emailValid = await userDB.findOne({ email });
+//   if (password != "") {
+//     const isValid = await bcrypt.compare(password, emailValid.password);
+//     if (!isValid) {
+//       return res.send({ msg: "Does not match your old password" });
+//     }
+//   }
+//   return res.send(emailValid._id);
+// });
+
+// ! email current check
+// router.post("/valid", async (req, res) => {
+//   const { email } = req.body;
+//   console.log(email);
+//   emailValid = await userDB.findOne({ email });
+//   console.log(emailValid);
+//   const find = await userDB.find({ __v: 0 });
+//   if (emailValid) {
+//     find.forEach((user) => {
+//       if (emailValid.email == email || user.email != email) {
+//         return res.send({ msg: "This email doesn’t belong to anyone" });
+//       } else {
+//         return res.send({ msg: "User already exists" });
+//       }
+//     });
+//   }
+// });
 router.put("/update", async (req, res) => {
-  const { name, surname, plate, email, password, _id } = req.body;
-  console.log(_id);
-  console.log(name, surname, plate, email, password);
+  const { name, surname, plate, email, password, _id, oldpassword } = req.body;
+  console.log(oldpassword, name, surname, plate, email, password, _id);
   const emailValid = await userDB.findOne({ _id });
   const find = await userDB.find({ __v: 0 });
   const hashedPassword = await bcrypt.hash(password, 15);
   const users = await userDB.findById(_id);
-  find.forEach(async (user) => {
-    if (emailValid.email == email || user.email != email) {
+
+  // ? password and email check
+  if (oldpassword != "") {
+    const isValid = await bcrypt.compare(oldpassword, emailValid.password);
+    if (!isValid) {
+      return res.send({ msg: "Does not match your old password" });
+    } else if (email == users.email) {
       return userDB.updateOne(
         { email: users.email },
         {
@@ -82,9 +104,33 @@ router.put("/update", async (req, res) => {
           },
         }
       );
-    } else if (user.email == email) {
-      return res.send({ msg: "User already exists" });
+    } else if (email) {
+      find.map((user) => {
+        if (user.email == email) {
+          return res.send({ msg: "This email belongs to any user" });
+        } else {
+          console.log(name, surname, email);
+          return userDB.updateOne(
+            { email: users.email },
+            {
+              $set: {
+                name: name,
+                surname: surname,
+                plate: plate,
+                email: email,
+                password: hashedPassword,
+              },
+            }
+          );
+        }
+      });
     }
-  });
+  }
+
+  // find.forEach(async (user) => {
+  //   if (emailValid.email == email || user.email != email) {
+  //     return
+  //   }
+  // });
 });
 module.exports = router;
